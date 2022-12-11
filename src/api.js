e03695e6f6368c988e14f8c2742ee0d1ca6708a1
boka96 <bjnbanovic@gmail.com>
2022-12-11 21:02:51 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e739a34e9emsh08007f3f7dd2efcp154b14jsn48ffb8795ff0',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ce76607eded0bd5280ea2b121fa81a5e";
